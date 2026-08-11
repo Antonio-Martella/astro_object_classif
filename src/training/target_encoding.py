f848a7fe6917b20fd1511f00a746492b9c13f3db
Antonio-Martella <zicr97@gmail.com>
2026-08-11 15:12:50 +0200

    Revisited the architecture and added input type checking.

--- src/training/target_encoding.py
+++ src/training/target_encoding.py
@@ -5,6 +5,8 @@ import joblib
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
+from src.utils.validate_type import validate_type
+
 
 def encode_targets_and_save(
     y_train: pd.Series, y_test: pd.Series, save: bool = False, path_encoder_save: Path | None = None
@@ -20,18 +22,22 @@ def encode_targets_and_save(
         tuple[pd.Series, pd.Series, LabelEncoder]: A tuple containing the encoded training labels,
                                                    encoded testing labels, and the fitted LabelEncoder.
     """
-    if save and path_encoder_save is None:
-        raise ValueError(
-            "Attention: it is not possible to save the target encoder locally. "
-            "Please check that the correct save path has been specified."
-        )
+    validate_type(
+        y_train=(y_train, pd.Series),
+        y_test=(y_test, pd.Series),
+        save=(save, bool),
+        path_encoder_save=(path_encoder_save, (Path, type(None))),
+    )
+
+    if not all(label in y_train.unique() for label in y_test.unique()):
+        raise ValueError("Attention: Some labels in the test set are not present in the training set!")
 
     label_encoder = LabelEncoder()
     y_train_encoded = pd.Series(label_encoder.fit_transform(y_train), index=y_train.index)
     y_test_encoded = pd.Series(label_encoder.transform(y_test), index=y_test.index)
 
     if save:
-        if save and path_encoder_save is None:
+        if path_encoder_save is None:
             raise ValueError(
                 "Attention: it is not possible to save the target encoder locally. "
                 "Please check that the correct save path has been specified."
@@ -49,5 +55,10 @@ def save_target_encoder(label_encoder: LabelEncoder, path: Path) -> None:
         label_encoder (LabelEncoder): The fitted LabelEncoder to be saved.
         path (Path): The path where the encoder should be saved.
     """
+    validate_type(
+        label_encoder=(label_encoder, LabelEncoder),
+        path=(path, Path),
+    )
     os.makedirs(path.parent, exist_ok=True)
-    joblib.dump(label_encoder, path)
+    with open(path, "wb") as file:
+        joblib.dump(label_encoder, file)
