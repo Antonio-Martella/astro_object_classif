08f7918beeca00ed5e85dea5746b34982daeb18e
Antonio-Martella <zicr97@gmail.com>
2026-08-11 16:19:01 +0200

    Added checks and validation on input variables

--- src/training/cross_validation.py
+++ src/training/cross_validation.py
@@ -4,9 +4,11 @@ import pandas as pd
 from sklearn.model_selection import StratifiedGroupKFold
 
 from configs.random_seed_loader import load_random_seed_config
+from configs.schemas import PreprocessingConfig
 from configs.schemas_loader import load_preprocessing_config
 from src.training.pipeline import build_training_pipeline
 from src.utils.metrics import evaluate_classification_metrics
+from src.utils.validate_type import validate_type
 
 
 def run_cross_validation(
@@ -18,72 +20,88 @@ def run_cross_validation(
     resampling_strategy: str = "class_weight",
     scaler_strategy: str = "standard",
     custom_params: dict[str, Any] | None = None,
+    preprocessing_config: PreprocessingConfig | None = None,
 ) -> dict[str, Any]:
     """
-    Esegue una Cross-Validation robusta (Stratified Group K-Fold) per valutare le performance di un modello.
+    Performs robust cross-validation (Stratified Group K-Fold) to evaluate a model's performance.
 
-    Per ogni fold, la funzione costruisce dinamicamente un'intera pipeline MLOps (ImbPipeline)
-    che include:
-    * Il preprocessing dei dati.
-    * Un eventuale ribilanciamento delle classi ('class_weight', 'smote' e 'undersampling')
-      configurabile nel file configs/... .
-    * L'addestramento del modello.
-    La validazione è strutturata in modo da prevenire il data leakage, assicurando che i record
-    appartenenti allo stesso gruppo (es. stesso 'field_ID') non vengano divisi tra set di
-    addestramento e validazione.
+    For each fold, the function dynamically builds an entire MLOps pipeline (ImbPipeline)
+    which includes:
+    * Data preprocessing.
+    * Possible class rebalancing ('class_weight', 'smote', and 'undersampling')
+    configurable in the configs/... file.
+    * Model training.
+    Validation is structured to prevent data leakage by ensuring that records
+    belonging to the same group (e.g., same 'field_ID') are not split between the training and validation sets.
 
     **Args**:
-        * *model_name* (str): L'identificativo testuale del modello da istanziare tramite ModelFactory
-                          (es. 'xgboost', 'random_forest', 'dense_nn', etc.).
-        * *X* (pd.DataFrame): Il dataset contenente le feature di addestramento.
-        * *y* (pd.Series): La variabile target (etichette).
-        * *groups* (pd.Series): La feature utilizzata per raggruppare i dati ed evitare leakage spaziale/temporale.
-        * *n_splits* (int, opzionale): Il numero di fold in cui dividere il dataset. Default a 5.
-        * *resampling_strategy* (str): Indica la strategia di resempling da applicare al dataset di training.
-                             Di default è 'None' che impica 'classe_weight' = 'balanced' per tutti i modelli.
-        * *custom_params* (dict, opzionale): Dizionario di iperparametri personalizzati (es. iniettati da Optuna).
-                                         Se None, il modello utilizzerà i parametri di default dal file config.
+    * *model_name* (str): The textual identifier of the model to be instantiated via ModelFactory
+                          (e.g., 'xgboost', 'random_forest', 'dense_nn', etc.).
+    * *X* (pd.DataFrame): The dataset containing the training features.
+    * *y* (pd.Series): The target variable (labels).
+    * *groups* (pd.Series): The feature used to group the data and avoid spatial/temporal leakage.
+    * *n_splits* (int, optional): The number of folds into which to split the dataset. Defaults to 5.
+    * *resampling_strategy* (str): Specifies the resemplifying strategy to apply to the training dataset.
+                                   The default is 'None', which implies 'classe_weight' = 'balanced' for all models.
+    * *custom_params* (dict, optional): Dictionary of custom hyperparameters (e.g., injected by Optuna).
+                                        If None, the model will use the default parameters from the config file.
+    * *preprocessing_config* (PreprocessingConfig, None): Preprocess configuration, necessary for the split train/test.
 
     **Returns**:
-        * *dict*: Un dizionario contenente la media aritmetica delle metriche di valutazione
-              (es. log_loss, accuracy, f1, precision, recall, roc-auc) calcolate su tutti i fold.
+    * *dict*: A dictionary containing the arithmetic mean of the evaluation metrics
+    (e.g., log_loss, accuracy, f1, precision, recall, roc-auc) computed over all folds.
     """
+    validate_type(
+        model_name=(model_name, str),
+        X=(X, pd.DataFrame),
+        y=(y, pd.Series),
+        groups=(groups, pd.Series),
+        n_splits=(n_splits, int),
+        resampling_strategy=(resampling_strategy, str),
+        scaler_strategy=(scaler_strategy, str),
+        custom_params=(custom_params, (dict, type(None))),
+        preprocessing_config=(preprocessing_config, (PreprocessingConfig, type(None))),
+    )
 
     if X.empty or y.empty:
-        raise ValueError("Attenzione i dataset passati alla cross validation sono vuoti!")
+        raise ValueError("Please note that the datasets passed to cross-validation are empty!")
 
     if len(X) != len(y):
-        raise ValueError("Attenzione le lunghezze dei dataset (X e y) nella cross validation non coincidono!")
+        raise ValueError("Please note that the lengths of the datasets (X and y) in the cross validation do not match!")
 
     if groups.empty:
-        raise ValueError("Attenzione gruppo passato alla cross validation vuoto!")
+        raise ValueError("Warning: Group passed to empty cross-validation!")
 
-    if isinstance(n_splits, int) is not True:
-        raise ValueError("Attenzione, lo split per la cross validation deve essere un intero!")
+    if len(groups) != len(X):
+        raise ValueError("The length of 'groups' series does not match the dataset X length!")
 
-    if isinstance(custom_params, dict) is not True and custom_params is not None:
-        raise TypeError(
-            "Attenzione lo spazio degli iperparametri fornita per l'ottimizzazione deve essere un dizionario!"
-        )
+    if groups.nunique() < n_splits:
+        raise ValueError(f"Number of unique groups ({groups.nunique()}) must be >= n_splits ({n_splits}).")
 
-    preprocessing_config = load_preprocessing_config()
+    if preprocessing_config is None:
+        preprocessing_config = load_preprocessing_config()
     random_seed_config = load_random_seed_config()
 
-    sgkf = StratifiedGroupKFold(n_splits=n_splits, shuffle=True, random_state=random_seed_config.random_seed_sgkf)
+    try:
+        sgkf = StratifiedGroupKFold(n_splits=n_splits, shuffle=True, random_state=random_seed_config.random_seed_sgkf)
+    except Exception as e:
+        raise RuntimeError(f"Attention: Stratified group k fold failed. Error {e}.") from e
 
     all_folds_metrics = []
 
     for train_idx, val_idx in sgkf.split(X, y, groups=groups):
-        X_fold_train, X_fold_val = pd.DataFrame(X).iloc[train_idx], pd.DataFrame(X).iloc[val_idx]
-        y_fold_train, y_fold_val = pd.Series(y).iloc[train_idx], pd.Series(y).iloc[val_idx]
+        X_fold_train, X_fold_val = X.iloc[train_idx], X.iloc[val_idx]
+        y_fold_train, y_fold_val = y.iloc[train_idx], y.iloc[val_idx]
 
-        pipeline = build_training_pipeline(
-            preprocessing_config, model_name, scaler_strategy, resampling_strategy, custom_params
-        )
+        try:
+            pipeline = build_training_pipeline(
+                preprocessing_config, model_name, scaler_strategy, resampling_strategy, custom_params
+            )
+            pipeline.fit(X_fold_train, y_fold_train)
+            fold_scores = evaluate_classification_metrics(pipeline, X_fold_val, y_fold_val)
+        except Exception as e:
+            raise RuntimeError(f"Attention: the cross-validation pipeline has stopped. Error {e}.") from e
 
-        pipeline.fit(X_fold_train, y_fold_train)
-
-        fold_scores = evaluate_classification_metrics(pipeline, X_fold_val, y_fold_val)
         all_folds_metrics.append(fold_scores)
 
     df_metrics = pd.DataFrame(all_folds_metrics)
