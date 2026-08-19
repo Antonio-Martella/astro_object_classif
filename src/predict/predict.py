e2b88230624fe971a89acf422fe90ad409873990
Antonio-Martella <zicr97@gmail.com>
2026-08-19 23:44:44 +0200

    feat: Updated predictive architecture. The core of the prediction is here.

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -1,118 +1,82 @@
+import json
 import logging
-import random
-import time
 
-import joblib
 import mlflow
+import numpy as np
 import pandas as pd
 from mlflow.tracking import MlflowClient
 from sklearn import set_config
 
-from configs.paths import DataPathConfig
-from src.utils.logger import setup_logger
+from configs.schemas_loader import load_cleaning_preprocessing_config, load_kaggle_config
+from src.data.preprocessing import build_stateless_cleaning_pipeline
+from src.utils.logger import restore_logging_after_mlflow
+from src.utils.validate_type import validate_type
 
 set_config(transform_output="pandas")
 
 
-setup_logger()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.propagate = True
 
 
 class AstroPredict:
-    def __init__(
-        self,
-        model_name: str | None = None,
-        model_version: str = "latest",
-        cleaner_name: str | None = None,
-        cleaner_version: str = "latest",
-    ):
+    def __init__(self, model_name: str | None = None, model_version: str = "latest"):
+        validate_type(model_name=(model_name, (str, type(None))), model_version=(model_version, str))
+
         client = MlflowClient()
-
-        setup_logger()
-        logger = logging.getLogger(__name__)
-
         self.model_uri = f"models:/{model_name}/{model_version}"
-        self.cleaner_uri = f"models:/{cleaner_name}/{cleaner_version}"
+        restore_logging_after_mlflow()
 
-        logger.info("Caricamento del modello previsionale da MLflow.")
-        self.model_pipeline = mlflow.sklearn.load_model(self.model_uri)
+        try:
+            logger.info(f"Loading model '{model_name}' (version: {model_version}) from MLflow...")
+            self.model_pipeline = mlflow.sklearn.load_model(self.model_uri)
 
-        logger.info("Caricamento della pipeline cleaner preprocessor da MLflow.")
-        self.cleaner_pipeline = mlflow.sklearn.load_model(self.cleaner_uri)
+            model_version_details = client.get_latest_versions(model_name)
+            encoder_local_path = mlflow.artifacts.download_artifacts(
+                run_id=model_version_details[0].run_id, artifact_path="encoder/label_encoder.json"
+            )
+            with open(encoder_local_path, "r", encoding="utf-8") as file:
+                self.label_encoder = json.load(file)
+        except Exception as e:
+            raise RuntimeError(f"Failed to load model artifacts from MLflow. Error: {e}") from e
 
-        logger.info("Caricamento del label encored del target da MLflow.")
-        model_version_details = client.get_latest_versions(model_name)
-        encoder_local_path = mlflow.artifacts.download_artifacts(
-            run_id=model_version_details[0]._run_id, artifact_path="encoder/target_encoder.pkl"
-        )
+        self.kaggle_config = load_kaggle_config()
+        self.clean_config = load_cleaning_preprocessing_config()
+        self.cleaning_pipeline = build_stateless_cleaning_pipeline(self.clean_config)
 
-        self.label_encoder = joblib.load(encoder_local_path)
+        self.expected_features = [col for col in self.kaggle_config.dataset_columns if col not in ["class", "field_ID"]]
+
+    def _validate_and_sanitize(self, X: pd.DataFrame) -> pd.DataFrame:
+        validate_type(X=(X, pd.DataFrame))
+
+        if "class" in X.columns:
+            X = X.drop(["class"], axis=1)
+        if "field_ID" in X.columns:
+            X = X.drop(["field_ID"], axis=1)
+
+        if sorted(list(X.columns)) != sorted(self.expected_features):
+            raise ValueError(
+                "Attention: The features of the provided datapoints do not match those of the original dataset"
+            )
+
+        return X
+
+    def _decode_predictions(self, raw_preds: np.ndarray) -> list[str]:
+        validate_type(raw_preds=(raw_preds, np.ndarray))
+
+        return [self.label_encoder["classes"][prediction] for prediction in raw_preds]
 
     def predict(self, X: pd.DataFrame):
-        X_copy = X.copy()
+        validate_type(X=(X, pd.DataFrame))
 
-        if "field_ID" in X.columns:
-            X_copy = X_copy.drop(["field_ID"], axis=1)
-        if "class" in X.columns:
-            X_copy = X_copy.drop(["class"], axis=1)
+        X_clean = self._validate_and_sanitize(X)
+        X_trans = self.cleaning_pipeline.transform(X_clean)
 
-        X_cleaned = self.cleaner_pipeline.transform(X_copy)
-        prediction = self.model_pipeline.predict(X_cleaned)
-        text_prediction = self.label_encoder.inverse_transform(prediction)
-        X_copy["prediction"] = text_prediction
+        preds = self.model_pipeline.predict(X_trans)
+        decoded_preds = self._decode_predictions(preds)
 
-        return X_copy
+        result_df = X.copy()
+        result_df["prediction"] = decoded_preds
 
-
-if __name__ == "__main__":
-    data_path = DataPathConfig()
-    mlflow.set_tracking_uri(f"sqlite:///{data_path.mlflow_db_path}")
-
-    MODEL_REGISTRY_NAME = "Classification_Astro_Model"
-    CLEANER_REFISTRY_NAME = "Cleaner_Astro_Pipeline"
-
-    logger.info("START")
-
-    predictor = AstroPredict(
-        model_name=MODEL_REGISTRY_NAME,
-        model_version="latest",
-        cleaner_name=CLEANER_REFISTRY_NAME,
-        cleaner_version="latest",
-    )
-
-    setup_logger()
-
-    logger = logging.getLogger(__name__)
-
-    logger.info("MODELLO CARICATO")
-
-    df = pd.read_csv(data_path.split_production_path)
-
-    logger.info("DATASET CARICATO")
-
-    logger.info("=" * 50)
-    logger.info("INZIO SIMULAZIONE DI PRODUZIONE")
-    logger.info("=" * 50)
-
-    while True:
-        time_lat = random.uniform(1.0, 2.0)
-        time.sleep(time_lat)
-
-        record_num = random.randint(0, len(df))
-        record = df.iloc[record_num : record_num + 1]
-
-        # prendo il tempo inziale
-        start_time = time.perf_counter()
-
-        prediction = predictor.predict(record)["prediction"]
-
-        # prendo il tempo finale
-        end_time = time.perf_counter()
-
-        logger.info(
-            f"\033[32m(true latency {round(end_time-start_time, 4)}s) \033[0m"
-            f"OSSERVAZIONE {record_num}: predizione --> "
-            f"\x1b[31m{prediction.iloc[0]} (true {df['class'].iloc[record_num]})\x1b[0m"
-        )
+        return result_df
